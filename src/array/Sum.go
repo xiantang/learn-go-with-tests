63b52398c60d9ef84ef9b08c9dd89a1073f88f24
xiantang <986494553@qq.com>
2019-08-29 09:52:22 +0800

    add sum_test and sum

--- src/array/Sum.go
+++ src/array/Sum.go
@@ -1,9 +1,10 @@
 package array
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum+=numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+
 }
--- src/array/sum_test.go
+++ src/array/sum_test.go
@@ -3,11 +3,16 @@ package array
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	// %v 用来展示数组
-	if want!= got {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		// 如果你使用[4]int 作为参数传入函数
+		// 不能通过编译 因为他们是不同的类型
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		// %v 用来展示数组
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
